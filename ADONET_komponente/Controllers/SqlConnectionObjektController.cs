cf4455782a201ea081b3ba78a907c1411d42d015
dklaic3006 <dklaic3006@outlook.com>
2019-04-09 00:34:30 +0200

    ado

---
+++ ADONET_komponente/Controllers/SqlConnectionObjektController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADONET_komponente.Controllers
+{
+    public class SqlConnectionObjektController : Controller
+    {
+        // GET: SqlConnectionObjekt
+        public ActionResult Index()
+        {
+            //prvo moramo sastaviti connection string
+            string connString = @"server = K-GRUPA-NOTEBOO\LOCALDB#FFEEEFA5";
+
+            //zatim instanciramo SqlConnection objekt
+            SqlConnection conn = new SqlConnection(connString);
+
+            // otvaramo vezu s bazom
+
+            try
+            {
+                conn.Open();
+                // ako je veza otvorena uspjeli smo se spojiti
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    Response.Write("Konekcija uspjela!");
+                }
+                
+            }
+            catch (SqlException sqlEx)
+            {
+                //u slučaju da konekcija nije uspjela, ispisujemo poruku o greški
+                Response.Write("Konekcija nije uspjela " + sqlEx.Message);
+            }
+            finally
+            {
+                //kad obavimo sve operacije u bazi konekciju je potrebno zatvoriti
+                conn.Close();
+            }
+
+
+            return View();
+        }
+    }
+}
